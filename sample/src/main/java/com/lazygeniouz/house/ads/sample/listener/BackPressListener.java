805873aa6e1022ea8736850034e244c9f30afbab
Darshan Pandya <itznotabug@gmail.com>
2019-02-22 20:32:39 +0530

    Push 'BackPressListener' to Git.

---
+++ sample/src/main/java/com/lazygeniouz/house/ads/sample/listener/BackPressListener.java
@@ -0,0 +1,7 @@
+package com.lazygeniouz.house.ads.sample.listener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public interface BackPressListener {
+    void onBackPressed(AppCompatActivity activity);
+}
